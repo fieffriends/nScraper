2abfbe43789ec6024b8d7604ec86c4926c91e0bd
fieffriends <48393262+fieffriends@users.noreply.github.com>
2023-05-08 22:41:09 +0200

    Update browser.js

--- browser.js
+++ browser.js
@@ -7,12 +7,13 @@ async function startBrowser(){
 	let browser;
 	try {
 	    console.log("Opening the browser......");
-	    browser = await puppeteer.launch({
-	        headless: true,
-            executablePath: executablePath(),
-	        args: ["--disable-setuid-sandbox"],
-	        'ignoreHTTPSErrors': true
-	    });
+	    browser = await puppeteer.connect({ browserWSEndpoint: 'wss://chrome.browserless.io?token=ff070955-1bf5-4b36-899d-212da94ca116' })
+	    //browser = await puppeteer.launch({
+	     //   headless: true,
+         //   executablePath: executablePath(),
+	   //     args: ["--disable-setuid-sandbox"],
+	  //      'ignoreHTTPSErrors': true
+	//    });
 	} catch (err) {
 	    console.log("Could not create a browser instance => : ", err);
 	}
